d9a2fe14bcca917dac9bb39730bf7bf6184257f4
Natalia Pavlova <natalja1912@yandex.ru>
2020-06-14 22:05:34 +0300

    fix: исправление ошибок _ ревью1

--- scripts/index.js
+++ scripts/index.js
@@ -8,19 +8,36 @@ let closeButton = popup.querySelector('.popup__close-button');
 let popupName = popup.querySelector('.popup__text_type_name');
 let popupJob = popup.querySelector('.popup__text_type_job');
 
-editButton.addEventListener('click', function () {
-    popup.classList.add('popup_opened');
-    popupName.placeholder = coverName.textContent;
-    popupJob.placeholder = coverJob.textContent;
-});
+//открытие и закрытие формы с редактированием профиля
 
-addButton.addEventListener('click', function () {
+function popupToggle(){
+    popup.classList.toggle('popup_opened');
+}
+
+popupToggle();
+
+
+editButton.addEventListener('click', popupToggle);
+closeButton.addEventListener('click', popupToggle);
+
+popupName.value = coverName.textContent;
+popupJob.value = coverJob.textContent;
+
+
+//загрузка данных из формы на страницу
+
+let popupForm = document.querySelector('.popup');
+
+let formSubmitHandler = function formSubmitHandler(evt){
+    evt.preventDefault();
     coverName.textContent = popupName.value;
     coverJob.textContent = popupJob.value;
-    popup.classList.remove('popup_opened');
-});
+    popupToggle();
+}
 
-closeButton.addEventListener('click', function () {
-    popup.classList.remove('popup_opened');
-});
+popupForm.addEventListener('submit', formSubmitHandler);
+
+
+
+
 
