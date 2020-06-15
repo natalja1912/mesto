c14e170c90c1b21eb9e4b9caf015e49198bd9888
Natalia Pavlova <natalja1912@yandex.ru>
2020-06-15 17:35:36 +0300

    fix: адаптивность формы

--- scripts/index.js
+++ scripts/index.js
@@ -7,22 +7,19 @@ let addButton = popup.querySelector('.popup__add-button');
 let closeButton = popup.querySelector('.popup__close-button');
 let popupName = popup.querySelector('.popup__text_type_name');
 let popupJob = popup.querySelector('.popup__text_type_job');
+let popupForm = document.querySelector('.popup');
 
 //открытие и закрытие формы с редактированием профиля
 
 function popupToggle(){
     popup.classList.toggle('popup_opened');
+    if (popup.classList.contains('popup_opened')) {
     popupName.value = coverName.textContent;
     popupJob.value = coverJob.textContent;
+    }
 }
 
-editButton.addEventListener('click', popupToggle);
-closeButton.addEventListener('click', popupToggle);
-
-
 //загрузка данных из формы на страницу
-
-let popupForm = document.querySelector('.popup');
 
 function formSubmitHandler(evt){
     evt.preventDefault();
@@ -31,6 +28,8 @@ function formSubmitHandler(evt){
     popupToggle();
 }
 
+editButton.addEventListener('click', popupToggle);
+closeButton.addEventListener('click', popupToggle);
 popupForm.addEventListener('submit', formSubmitHandler);
 
 
