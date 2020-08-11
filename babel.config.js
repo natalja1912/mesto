154527075fd4e03f7c7ce055727e6cdaaffa555e
Natalia Pavlova <natalja1912@yandex.ru>
2020-08-11 23:44:34 +0300

    fix: webpack

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,18 @@
 const presets = [
-      [
-        "@babel/env",
-        {
-          "targets": {
-            "edge": "17",
-            "firefox": "60",
-            "chrome": "67",
-            "safari": "11.1"
-          },
-          "useBuiltIns": "usage",
-          "corejs": "3.6.4"
-        }
-      ],
-      {"plugins": [
-        ["transform-class-properties", { "spec": true }]
-      ]}
-    ]
-    module.exports = { presets };
+    ['@babel/env', { 
+      targets: { 
+        edge: '17',
+        ie: '11',
+        firefox: '50',
+        chrome: '64',
+        safari: '11.1'
+      },
+  
+      useBuiltIns: "entry"
+    }],
+    {
+        "plugins": ["@babel/plugin-proposal-class-properties"]
+      }
+  ];
+  
+  module.exports = { presets };
